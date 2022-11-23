4b1141c4f6b23362a1edd33bc823fd6f7f631f7f
Jamison Griffith <jamison@zingstudios.com>
2022-11-22 17:18:08 -0700

    Sets default git branch; removes extraneous build packages

--- docker/.build/symfony-init.sh
+++ docker/.build/symfony-init.sh
@@ -3,6 +3,7 @@
 if [ "$1" = "init" ]; then
   git config --global user.name "$2" && \
   git config --global user.email "$3" && \
+  git config --global init.defaultBranch main && \
   symfony new /home/zing/dev --webapp
 else
   mkdir /home/zing/dev
